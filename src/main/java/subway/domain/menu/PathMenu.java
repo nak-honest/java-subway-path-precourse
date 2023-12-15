0c95b69185e864d1fecfcf883ab781c31abe873d
nhlee98 <e5slnh9876@gmail.com>
2023-12-15 10:19:26 +0900

    feat: 경로 기준 메뉴 기능이 1, 2, B 중 하나가 아니라면 예외를 발생시킨다

---
+++ src/main/java/subway/domain/menu/PathMenu.java
@@ -0,0 +1,22 @@
+package subway.domain.menu;
+
+import java.util.Arrays;
+
+public enum PathMenu {
+    LOOKUP_BY_DISTANCE("1"),
+    LOOKUP_BY_TIME("2"),
+    BACK("B");
+
+    private final String menu;
+
+    PathMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public static PathMenu of(String menu) {
+        return Arrays.stream(PathMenu.values())
+                .filter(pathMenu -> pathMenu.menu.equals(menu))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 메인 메뉴는 1, 2, B 중 하나여야 합니다."));
+    }
+}
---
+++ src/test/java/subway/menu/PathMenuTest.java
@@ -0,0 +1,26 @@
+package subway.menu;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import subway.domain.menu.MainMenu;
+import subway.domain.menu.PathMenu;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PathMenuTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"q", "Q", "0", " ", "3"})
+    void 경로_조회_메뉴가_1_2_B_중_하나가_아니라면_예외를_발생시킨다(String menu) {
+        assertThatThrownBy(() -> PathMenu.of(menu))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 메인 메뉴는 1, 2, B 중 하나여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "B"})
+    void 경로_조회_메뉴가_1_2_B_중_하나라면_예외를_발생시키지_않는다(String menu) {
+        assertThatCode(() -> PathMenu.of(menu))
+                .doesNotThrowAnyException();
+    }
+}
