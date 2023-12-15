d195028e5043742ef9524938d9cb759b42e49abc
nhlee98 <e5slnh9876@gmail.com>
2023-12-15 11:42:15 +0900

    refactor: 패키지 이동

--- src/main/java/subway/repository/StationRepository.java
+++ src/main/java/subway/repository/StationRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
+    private StationRepository() {}
+
     private static final List<Station> stations = new ArrayList<>();
 
     public static List<Station> stations() {
--- src/test/java/subway/domain/menu/MainMenuTest.java
+++ src/test/java/subway/domain/menu/MainMenuTest.java
@@ -1,4 +1,4 @@
-package subway.menu;
+package subway.domain.menu;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
